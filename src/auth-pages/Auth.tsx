4af6c9502bf795b67a2436464de7111b467b8f95
Louffty <galagtik10@gmail.com>
2024-04-09 17:31:01 +0300

    feat: add example of login page

--- src/auth-pages/Auth.tsx
+++ src/auth-pages/Auth.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import global from "../global-static/styles/global.module.css";
+import Navbar from "../main/App";
+import "./static/Auth.css";
+
+function AuthContainer() {
+    return (
+        <>
+            <div className="login-container">
+                <div className="login-block">
+                    <span className={global.bigtext}>
+                        Войти
+                    </span>
+                    <input type="email" name="asd" id="email" className="input-f" />
+                </div>
+            </div>
+        </>
+    );
+}
 
 function Auth() {
     return (
         <>
-            <h1>aaaaaa</h1>
+            <div className={global.container}>
+                <Navbar.NavBar></Navbar.NavBar>
+                <AuthContainer></AuthContainer>
+            </div>
         </>
     );
 }
